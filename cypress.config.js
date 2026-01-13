802e4ce636c1d1518f6eb2b16c647c48aa76fc58
Pedro Andrade <pedrohandradeg@gmail.com>
2026-01-12 21:51:01 -0300

    adicionando github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "g6xazy",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
